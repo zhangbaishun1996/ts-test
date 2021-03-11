0e6836c0b99d9df66d55b5308843b9b06ceaa4f2
张柏顺 <baishun.zhang@hkbitex.com.hk>
2021-03-11 18:42:05 +0800

    webpack

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,8 @@ ax = 10
 let objx:{name:string}
 objx = {name:"age"}
 console.log(objx.name)
-objx.name = "19"
+objx.name = "19" 
 console.log(objx.name)
 console.log(ax,hi)
+console.log(Promise)
 console.log(Promise)
